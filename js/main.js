0c6c3e42ffbce1616db012e6b302ba2241e52786
Wanjugu Irene <ireneemaina@gmail.com>
2016-07-27 10:08:56 +0300

    Inside out with Foundation Framework

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+    $('.fadein :first-child').fadeOut(2000)
+    .next('img').fadeIn(2000)
+    .end().appendTo('.fadein');},
+    7000);
+});
